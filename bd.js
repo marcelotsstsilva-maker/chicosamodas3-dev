6efda0798e7aa2c742e294a344a581b0bfc9d3e8
marcelotsstsilva-maker <marcelo.tsst.silva@gmail.com>
2025-10-29 09:04:03 -0300

    Add files via upload

--- bd.js
+++ bd.js
@@ -1,13 +1,17 @@
-// bd.js
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-  host: "btrjdgtvkjxwwdhabaxu-mysql.services.clever-cloud.com",
-  port: 3306,
-  user: "ugqdqh8g9q15zwfr",
-  password: "qimE9z4Wbqs13i7Q2PS8",
-  database: "btrjdgtvkjxwwdhabaxu",
+  host: "gondola.proxy.rlwy.net",
+  port: 22200,
+  user: "root",
+  password: "KvFWKQNwfssKKUbAJnvLrrxFWoyrVMJM",
+  database: "railway",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false, // necessário no Render
+  },
 });
+
+
